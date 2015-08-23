34af7ea38d6daa19d4713a0760301b2c1c87951f
michidk <michidk@live.de>
2015-08-23 02:50:27 +0200

    added vignette shader

---
+++ Assets/Shader/Vignette.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Default 
+{
+    [ExecuteInEditMode]
+    public class Vignette : MonoBehaviour
+    {
+        [Range(0, 10)]
+        public float Power = 5.0f;
+
+        private Material mat;
+
+        void Awake()
+        {
+            mat = new Material(Shader.Find("Custom/Vignette"));
+        }
+
+        void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            if (Power <= 0)
+            {
+                Graphics.Blit(source, destination);
+            }
+            else
+            {
+                mat.SetFloat("_Power", Power);
+                Graphics.Blit(source, destination, mat);
+            }
+        }
+    }
+}
---
+++ Assets/Shader/Vignette.shader
@@ -0,0 +1,30 @@
+﻿Shader "Custom/Vignette" {
+	Properties {
+		_MainTex ("Albedo (RGB)", 2D) = "white" {}
+		_Power("Power", Float) = 5.0
+	}
+	SubShader {
+		Pass {
+			CGPROGRAM
+
+			#pragma vertex vert_img
+			#pragma fragment frag
+
+			#include "UnityCG.cginc"
+
+			sampler2D _MainTex;
+			float _Power;
+
+			float4 frag(v2f_img v) : COLOR
+			{
+				float2 uv = v.uv;
+				float4 tex = tex2D(_MainTex, uv);
+				tex *= 1 - dot(uv - 0.5f, uv - 0.5f) * _Power;
+				return tex;
+			}
+
+			ENDCG
+		}
+	} 
+	FallBack "Diffuse"
+}
